fcf2017997546629f685d7d68b02b9e59facc9b7
Andrew Boyd <andrew.d.boyd@gmail.com>
2019-12-22 08:04:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,18 @@
 const vandium = require('vandium');
 const mysql   = require('mysql');
 
+var pool  = mysql.createPool({
+  connectionLimit : 100,
+  host            : process.env.rds_host,
+  user            : process.env.rds_user,
+  password        : process.env.rds_password,
+  database        : process.env.rds_database,
+  port            : process.env.rds_port
+});
+
 exports.handler = vandium.generic()
     .handler( (event, context, callback) => {
 
-  let connection = mysql.createConnection({
-       host     : '[rds_host]',
-       user     : '[rds_user]',
-       password : '[rds_password]',
-       database : '[rds_database]'
-  });
   let sql = "UPDATE items SET ";
   sql = sql + " name = " + connection.escape(event.name);
   sql = sql + " racing_name = " + connection.escape(event.racing_name);
@@ -22,7 +25,7 @@ exports.handler = vandium.generic()
 
   console.log('UpdateCompany SQL: ${sql}');
 
-  connection.query(sql, function (error, results, fields) {
+  pool.query(sql, function (error, results, fields) {
     let response = {};
     response['item_id'] = event.item_id;
     response['name'] = event.name;
